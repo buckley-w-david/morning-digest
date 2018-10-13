40e82d96e910892a0fd2164f49599790573783f5
David Buckley <buckley.w.david@gmail.com>
2018-10-13 15:15:24 -0400

    add ALL special component option

--- morning_digest/cli.py
+++ morning_digest/cli.py
@@ -16,13 +16,19 @@ def main():
 # Come up with a method fo specifying alternate URLS
 # Instead of always pulling the default
 async def a_generate(output, components_names):
-    types = [ComponentType.from_name(c) for c in components_names]
+    if 'ALL' in components_names:
+        types = [c for c in ComponentType]
+    else:
+        seen = set()
+        # Remove duplicates while maintaing order
+        types = [ComponentType.from_name(c) for c in components_names if not (c in seen or seen.add(c))]
+
     components = [await TYPE_MAP[t].default() for t in types]
     output.write(render.render_components(components))
 
 
 @main.command()
 @click.argument("output", type=click.File("w"))
-@click.option("--component", type=click.Choice(ALL_COMPONENTS), multiple=True)
+@click.option("--component", type=click.Choice(ALL_COMPONENTS + ['ALL']), multiple=True)
 def generate(output: typing.IO[str], component):
     asyncio.run(a_generate(output, component))
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-digest generate --component MONKEYUSER --component XKCD - | $DIR/send.sh "${1:-$(git config --get user.email)}"
+digest generate --component ALL - | $DIR/send.sh "${1:-$(git config --get user.email)}"
